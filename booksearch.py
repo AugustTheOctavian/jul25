06fc4d555af96ee66e0e57f433004c4cf0f5d711
butterbastedsteak <bharathlivingston@gmail.com>
2026-07-26 11:11:23 +0530

    json

--- booksearch.py
+++ booksearch.py
@@ -2,6 +2,27 @@ import json
 with open("books.json", "r") as file:
   books = json.load(file)
 
-print(type(books))
-for book, author in books.items():
-  print(book.upper() , author.title())
+  
+# booksearch = input("enter book name: ").lower()
+# if booksearch in books:
+#   print(booksearch, books["booksearch"])
+
+lowercasebooks = []
+  
+for book in books:
+  lowercasebooks.append(book)
+print(lowercasebooks)
+
+searchbook = input("enter book: ").lower()
+if searchbook in lowercasebooks:
+  print(f"yes, found  at {lowercasebooks.index(searchbook)+1}.")
+
+# # print(books)
+# for book in books:
+#   print(book.lower().replace(' ',''))
+
+# searchbook = input("enter a book: ").lower()
+# if searchbook in books:
+#   print(searchbook)
+
+  
